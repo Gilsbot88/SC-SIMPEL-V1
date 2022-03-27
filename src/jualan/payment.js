0ac6d6f417a78acb49a5d7d50d1cbbd1478e13b3
Gilsbot88 <101207495+Gilsbot88@users.noreply.github.com>
2022-03-27 22:50:35 +0700

    Update payment.js

--- src/jualan/payment.js
+++ src/jualan/payment.js
@@ -1,10 +1,10 @@
 exports.payment = (prefix) => {
 return  `*- PAYMENT*
 
-• GOPAY : 628xx-xxxx-xxx
-• DANA : 628xx-xxxx-xxxx
-• OVO : 628xx-xxxx-xxxx
-• QRIS ALLPAY : _Chat owner_
+• GOPAY : 62813-1891-0218
+• DANA : 62831-3689-0848
+• OVO : TIDAK TERSEDIA
+• QRIS ALLPAY : _wa.me/6283136890848_
 
 _Untuk pembayaran via qris silakan chat owner untuk meminta qris_
 _Jika sudah di transfer harap kirim bukti transfer ke owner bot !!_`
