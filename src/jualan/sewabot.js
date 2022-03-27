3d4f788dd65eda9e33f1765b50ad62a7201d28f1
Gilsbot88 <101207495+Gilsbot88@users.noreply.github.com>
2022-03-27 22:54:09 +0700

    Update sewabot.js

--- src/jualan/sewabot.js
+++ src/jualan/sewabot.js
@@ -1,9 +1,9 @@
 exports.sewabot = (prefix) => {
 return  `*- SEWABOT*
 
-• 1 MINGGU : 5.000
-• 1 BULAN : 10.000
-• PERMANEN : 15.000
+• 1 MINGGU : 15.000
+• 1 BULAN : 25.000
+• PERMANEN : 40.000
 
 _Untuk pembayaran silakan ketik_ *${prefix}payment*
 _Untuk kalian yang minat silakan hubungin owner bot_`
