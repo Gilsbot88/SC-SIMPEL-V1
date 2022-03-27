1376bc94b10e2f250679d84df4b2cd07b0b858f4
Gilsbot88 <101207495+Gilsbot88@users.noreply.github.com>
2022-03-27 22:57:28 +0700

    Update groupowner.js

--- src/groupowner.js
+++ src/groupowner.js
@@ -1,9 +1,9 @@
 exports.groupowner = (prefix) => {
 return `*- LINK GROUP OWNER BOT*
 
-1. https://chat.whatsapp.com/G1PlSE3VR6e99hmMaxzpkg
-2. https://chat.whatsapp.com/LV3DOUhJerlHBkvAjcB5zr
-3. https://chat.whatsapp.com/LNCchYC5Iyp2VLpqy0oK7C
+1. https://chat.whatsapp.com/I3uvRkj0vGF0yexwy1OnUv
+2. https://chat.whatsapp.com/J8jRHj8kyim2QEJvQSklKf
+3. https://chat.whatsapp.com/Jvp57NiL4yq4dnOHr2x37c
 
 _Jika mau bergabung jangan lupa baca deskripsi!!_`
 }
